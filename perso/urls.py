48317e9d7717cbc3275ed61551515d25632714a1
Arthur Rio <arthur@punchtab.com>
2014-01-27 21:31:40 -0800

    upgrade to django 1.6 and start building the printable version of the resume

--- perso/urls.py
+++ perso/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls.defaults import patterns
+from django.conf.urls import patterns
+from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('perso.views',
-    (r'^$', 'index'),
-    (r'^robots.txt$', 'robots'),
+    (r'^$', TemplateView.as_view(template_name='index.html')),
+    (r'^resume/$', TemplateView.as_view(template_name='resume.html')),
+    (r'^robots.txt$', TemplateView.as_view(template_name='robots.html')),
+    (r'^humans.txt$', TemplateView.as_view(template_name='humans.html')),
 )
 
